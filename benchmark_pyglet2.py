4439c4768b64aa2b690b243d24871d81521b6229
condac <burns.nu@gmail.com>
2021-11-10 17:20:51 +0100

    test

--- benchmark_pyglet2.py
+++ benchmark_pyglet2.py
@@ -16,6 +16,7 @@ import argparse
 #glEnable(GL_TEXTURE_2D)         # enable textures
 #glShadeModel(GL_SMOOTH)         # smooth shading of polygons
 
+
 full = False
 # main loop
 
@@ -30,9 +31,9 @@ else:
   screen = screens[0]
 
 
-config = pyglet.gl.Config(sample_buffers=1, samples=1, depth_size=24, double_buffer=True)
+config = pyglet.gl.Config(depth_size=24, double_buffer=True)
 #window = pyglet.window.Window(config=config, resizable=True, width = 1024, height=768, screen=screen, fullscreen=full)
-window = pyglet.window.Window(config=config, resizable=True,  screen=screen, fullscreen=full)
+window = pyglet.window.Window(config=config, resizable=True, width = 1024, height=768, screen=screen, fullscreen=full)
 glEnable(GL_LINE_SMOOTH)
 glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE)
 glEnable(GL_BLEND)                                  # transparency
@@ -53,6 +54,66 @@ frames = 0
 t0 = time.time()
 w = 1024
 h = 768
+
+
+pic = pyglet.image.load('512.png')
+texture = pic.get_texture()
+
+radie = 45
+ballsize = 10.0
+balldepth = 122.0
+heading = 0.0
+tilt = 0.0
+rota = 0.0
+
+def draw_sphere():
+    global heading, radie, tilt, rota
+    global ballsize
+
+
+    #glClearDepth(0.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+             # enable depth testing
+
+    # reset modelview matrix
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    #aspectRatio = window.height / window.width
+    #1.0 * width / height
+    #gluPerspective(45, aspectRatio, 0.1, 100)
+    gluPerspective(40, 1.0 * window.width / window.height, 1, balldepth)
+
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    pan = 4.0
+    glLoadIdentity();
+    roll =   np.deg2rad(rota)
+    tiltrad = np.deg2rad(tilt)
+    #gluLookAt (math.cos(tiltrad)*radie, 0.0, math.sin(tiltrad)*radie+pan, 0.0, 0.0, pan,0.0 , math.sin(roll), math.cos(roll));
+
+    gluLookAt (0.0, radie, pan, 0.0, 0.0, pan,0.0 , 0.0, 1.0);
+    glRotatef(rota, 0.0, 1.0, 0.0)
+    glRotatef(-tilt+5, 1.0, 0.0, 0.0)
+    glRotatef(-heading+179, 0.0, 0.0, 1.0)
+
+    #setColor(colorGreenLight)
+    glEnable(texture.target)        # typically target is GL_TEXTURE_2D
+    glBindTexture(texture.target, texture.id)
+
+    q = gluNewQuadric()
+    gluQuadricOrientation(q,GLU_OUTSIDE)
+    gluQuadricDrawStyle(q,GLU_FILL)
+    gluQuadricTexture(q, GL_TRUE)
+
+    gluSphere(q,ballsize,50,50)
+
+    glDisable(texture.target)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glTranslatef(100.0, 0.0, 0.0)
+
 
 def square():
     glBegin(GL_QUADS)
@@ -88,6 +149,7 @@ def on_draw():
     iterate()
     glColor3f(1.0, 0.0, 3.0)
     square()
+    draw_sphere()
     #glutSwapBuffers()
     frames += 1
     t = time.time()
